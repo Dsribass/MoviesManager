0068f4359e9bc1c1e70b62ac5d7e9373589bb838
Daniel Ribas <ds_ribas@hotmail.com>
2022-12-02 20:09:36 -0300

    feat: integrate movie detail view

--- app/src/main/java/com/example/moviesmanager/presentation/movies/views/MovieDetailFragment.kt
+++ app/src/main/java/com/example/moviesmanager/presentation/movies/views/MovieDetailFragment.kt
@@ -7,8 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.moviesmanager.data.repository.MovieRepository
 import com.example.moviesmanager.databinding.FragmentMovieDetailBinding
+import com.example.moviesmanager.model.Movie
 import com.example.moviesmanager.utils.Factory
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.SingleObserver
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import io.reactivex.rxjava3.subjects.SingleSubject
 import java.util.*
 
 class MovieDetailFragment : Fragment() {
@@ -32,10 +38,26 @@ class MovieDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        repository.getMovie(id)
+            .observeOn(Schedulers.io())
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .subscribe { movie, _ -> showMovieDetails(movie) }
+    }
+
+    private fun showMovieDetails(movie: Movie) {
+        movie.run {
+            binding.movieDetailTitleText.text = name
+            binding.movieDetailReleaseDateText.text = releaseDate.toString()
+            binding.movieDetailStudioText.text = studio
+            binding.movieDetailDurationText.text = duration.toString()
+            binding.movieDetailGenreText.text = movie.genre.name
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        disposables.clear()
     }
 }
