8c8b4b7407b1f955416c0316c2a0fc44fbf5f11f
Daniel Ribas <ds_ribas@hotmail.com>
2022-12-04 20:33:32 -0300

    feat: implement rate movie functionality

--- app/src/main/java/com/example/moviesmanager/presentation/movies/views/MovieDetailFragment.kt
+++ app/src/main/java/com/example/moviesmanager/presentation/movies/views/MovieDetailFragment.kt
@@ -1,20 +1,25 @@
 package com.example.moviesmanager.presentation.movies.views
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.moviesmanager.R
 import com.example.moviesmanager.data.repository.MovieRepository
 import com.example.moviesmanager.databinding.FragmentMovieDetailBinding
+import com.example.moviesmanager.databinding.RateMovieDialogContentBinding
 import com.example.moviesmanager.model.Movie
 import com.example.moviesmanager.utils.Factory
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.SingleObserver
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.schedulers.Schedulers
-import io.reactivex.rxjava3.subjects.BehaviorSubject
-import io.reactivex.rxjava3.subjects.SingleSubject
+import io.reactivex.rxjava3.subjects.PublishSubject
 import java.util.*
 
 class MovieDetailFragment : Fragment() {
@@ -26,6 +31,10 @@ class MovieDetailFragment : Fragment() {
 
     private lateinit var repository: MovieRepository
     private val disposables = CompositeDisposable()
+    private val onConfirmMovieRateSubject = PublishSubject.create<Int>()
+    private val onRefreshSubject = PublishSubject.create<Unit>()
+
+    private var movieDetail: Movie? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,11 +47,62 @@ class MovieDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupObservables()
+    }
 
-        repository.getMovie(id)
-            .observeOn(Schedulers.io())
-            .subscribeOn(AndroidSchedulers.mainThread())
-            .subscribe { movie, _ -> showMovieDetails(movie) }
+    private fun setupObservables() {
+        Observable.merge(
+            listOf(
+                Observable.just(Unit),
+                onRefreshSubject
+            )
+        )
+            .flatMapSingle { repository.getMovie(id) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    movieDetail = it
+                    showMovieDetails(it)
+                },
+                {
+                    Toast.makeText(
+                        binding.root.context,
+                        it.localizedMessage,
+                        Toast.LENGTH_LONG
+                    ).show()
+                },
+            )
+            .addTo(disposables)
+
+        binding.rateMovieButton.setOnClickListener {
+            createRateMovieDialog {
+                if (it.text.isEmpty()) {
+                    it.setText("0")
+                }
+                onConfirmMovieRateSubject.onNext("${it.text}".toInt())
+            }
+        }
+
+        onConfirmMovieRateSubject
+            .subscribe {
+                if (movieDetail != null) {
+                    repository.updateMovie(
+                        Movie(
+                            id = movieDetail!!.id,
+                            name = movieDetail!!.name,
+                            studio = movieDetail!!.studio,
+                            duration = movieDetail!!.duration,
+                            releaseDate = movieDetail!!.releaseDate,
+                            genre = movieDetail!!.genre,
+                            rate = it,
+                        )
+                    ).subscribe {
+                        onRefreshSubject.onNext(Unit)
+                    }
+                        .addTo(disposables)
+                }
+            }
+            .addTo(disposables)
     }
 
     private fun showMovieDetails(movie: Movie) {
@@ -52,6 +112,11 @@ class MovieDetailFragment : Fragment() {
             binding.movieDetailStudioText.text = studio
             binding.movieDetailDurationText.text = duration.toString()
             binding.movieDetailGenreText.text = movie.genre.name
+            if (wasWatched) {
+                binding.movieDetailRateContent.visibility = View.VISIBLE
+                binding.movieDetailRate.text = "$rate/10"
+                binding.rateMovieButton.visibility = View.GONE
+            }
         }
     }
 
@@ -59,5 +124,19 @@ class MovieDetailFragment : Fragment() {
         super.onDestroyView()
         _binding = null
         disposables.clear()
+    }
+
+    private fun createRateMovieDialog(onConfirmButton: (EditText) -> Unit) {
+        val builder = AlertDialog.Builder(context)
+        val content = RateMovieDialogContentBinding.inflate(layoutInflater, null, false)
+        builder.setMessage(R.string.rate)
+            .setView(content.root)
+            .setPositiveButton(
+                R.string.confirm
+            ) { _, _ ->
+                onConfirmButton(content.rateContentEditText)
+            }
+            .setNegativeButton(R.string.cancel) { _, _ -> }
+        builder.show()
     }
 }
